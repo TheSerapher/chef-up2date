3babd8101a37efe9bb5f59e5d60feaf526e80543
Sebastian Grewe <sebastian.grewe@xing.com>
2015-03-19 09:14:26 +0100

    [ISSUE-6] Updated documentation and version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name              "up2date"
 maintainer        "Sebastian Grewe"
 maintainer_email  "sebastian.grewe@gmail.com"
 license           "Apache 2.0"
-description       "Runs a repository cache update up2dated on distribution."
+description       "Runs a repository cache update on supported distributions."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.1"
+version           "1.0.0"
 recipe            "up2date::default", "Update system package and repository caches."
 
 %w( ubuntu debian rhel centos ).each do |sup|
